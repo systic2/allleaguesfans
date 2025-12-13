6a6b001b365c054205a3bb5333961126413d3495
systic <llsysticll@gmail.com>
2025-12-13 22:29:05 +0900

    feat(ui): remove '3-API 통합 데이터' text from LeagueCard

--- src/pages/LeagueListPage.tsx
+++ src/pages/LeagueListPage.tsx
@@ -125,12 +125,6 @@ function LeagueCard({ league }: { league: LeagueLite }) {
         <div className="mt-4 pt-4 border-t border-slate-700/50 mt-auto">
           <div className="flex items-center justify-between text-xs text-slate-400">
             <span>{getLeagueSeasonDisplay(league.slug)}</span>
-            <span className="flex items-center">
-              <svg className="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
-              </svg>
-              3-API 통합 데이터
-            </span>
           </div>
         </div>
       </div>
